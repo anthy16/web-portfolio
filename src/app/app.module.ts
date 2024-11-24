c909e37141bb4c7796fba6f17f869ca944bc2157
Anders Thygesen <anderskirsby@gmail.com>
2024-11-24 20:34:43 +0100

    chore: added ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { SettingsModule } from './settings/settings.module';
 import { StoreModule } from '@ngrx/store';
 import { layoutReducer } from './store/reducers/layout.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +17,11 @@ import { layoutReducer } from './store/reducers/layout.reducer';
     AppRoutingModule,
     SettingsModule,
     SharedModule,
-    StoreModule.forRoot({ layout: layoutReducer }, {})
+    StoreModule.forRoot({ layout: layoutReducer }, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
